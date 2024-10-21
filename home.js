c3e6f5521bd61ecfc24cc9db8e47382793b0aefa
dharmesh2712 <85621362+dharmesh2712@users.noreply.github.com>
2024-10-21 22:25:58 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,7 @@
+document.getElementById('login-popup').addEventListener('click',function(){
+ document.getElementById('loginModal').style.display = 'flex'    
+});
+
+function closePopup(){
+    document.getElementById('loginModal').style.display = 'none'
+}
